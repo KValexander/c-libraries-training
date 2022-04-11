1c6836f369d4893fe233fc29c76b26d0eaf024bf
KValexander <ewoase@gmail.com>
2022-04-11 16:25:48 +0300

    basis upd

--- 1Basis/src/main.c
+++ 1Basis/src/main.c
@@ -1,35 +1,124 @@
 // Стандартные библиотеки
 #include <stdio.h>
+#include <time.h>
 // Внешние библиотеки
 #include <SDL.h>
 #include <SDL_image.h>
 
+// Константы
+#define GRAVITY 0.35f
+
+// Структура Текстуры
+typedef struct {
+    SDL_Texture *player_frames[2];
+    SDL_Texture *ledge;
+    SDL_Texture *star;
+} Textures;
+
 // Структура Игрок
 typedef struct {
-    int x, y;   // позиция
-    int width;  // ширина
-    int height; // высота
+    float x, y;   // позиция
+    int w, h;   // ширина и высота
+    float dx, dy; // скорость
     short life; // очки жизни
     char *name; // имя
-    SDL_Texture *texture; // Текстура
+    int on_ledge; // проверка коллизии
 } Player;
+
+// Структура Звезда
+typedef struct Star{
+    int x, y;
+} Star;
+
+// Структура Уступ
+typedef struct {
+    int x, y, w, h;
+} Ledge;
 
 // Структура Состояние игры
 typedef struct {
+    Textures textures;  // структура Текстуры
+    
     Player player;      // структура Игрок
+    Star stars[100];    // массив структур Звезда
+    Ledge ledges[100];  // массив структур Уступ 
+
     SDL_Renderer *rndr; // визуализатор
 } GameState;
 
+// Загрузка игры
+void load_game(GameState *game) {
+    SDL_Surface *surface = NULL; // поверхность
+
+    // Загрузка текстур
+    // Звезда
+    surface = IMG_Load("assets/star.png");
+    if(surface == NULL) printf("Image not found");
+    game->textures.star = SDL_CreateTextureFromSurface(game->rndr, surface);
+    SDL_FreeSurface(surface); // очистка поверхности
+    // Уступ
+    surface = IMG_Load("assets/ledge.png");
+    if(surface == NULL) printf("Image not found");
+    game->textures.ledge = SDL_CreateTextureFromSurface(game->rndr, surface);
+    SDL_FreeSurface(surface); // очистка поверхности
+    // Фреймы игрока
+        // Право
+    surface = IMG_Load("assets/right.png");
+    game->textures.player_frames[0] = SDL_CreateTextureFromSurface(game->rndr, surface);
+    SDL_FreeSurface(surface); // очистка поверхности
+        // Лево
+    surface = IMG_Load("assets/left.png");
+    game->textures.player_frames[1] = SDL_CreateTextureFromSurface(game->rndr, surface);
+    SDL_FreeSurface(surface); // очистка поверхности
+
+    // Инициализация игрока
+    game->player.x = 320-24;
+    game->player.y = 240-24;
+    game->player.w = 48;
+    game->player.h = 48;
+    game->player.dx = 0;
+    game->player.dy = 0;
+    game->player.on_ledge = 0;
+
+    // Инициализация звёзд
+    for(int i = 0; i < 100; i++) {
+        game->stars[i].x = rand() % 640;
+        game->stars[i].y = rand() % 480;
+    }
+
+    // Инициализация уступов
+    for(int i = 0; i < 100; i++) {
+        game->ledges[i].w = 256;
+        game->ledges[i].h = 64;
+        game->ledges[i].x = i*256;
+        game->ledges[i].y = 400;
+    }
+    game->ledges[99].x = 350;
+    game->ledges[99].y = 200;
+}
+
 // Обработка событий
-int game_events(GameState *game_state) {
+int game_events(GameState *game) {
     SDL_Event event;
     int quit = 0;
 
     // Цикл обработки событий
     while(SDL_PollEvent(&event)) {
         switch(event.type) {
-            // Выход из приложения
-            case SDL_QUIT: quit = 1; break;
+            case SDL_QUIT: quit = 1; break; // выход из приложения
+
+            // Нажатия на клавиши
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+                    case SDLK_UP:
+                        if(game->player.on_ledge) {
+                            game->player.dy = -12;
+                            game->player.on_ledge = 0;
+                        }
+                    break;
+                }
+            break;
+
         }
     }
 
@@ -37,63 +126,105 @@ int game_events(GameState *game_state) {
     const Uint8 *state = SDL_GetKeyboardState(NULL);
     // Движение влево
     if(state[SDL_SCANCODE_LEFT])
-        game_state->player.x -= 10;
+        game->player.x -= 10;
     // Движение вправо
-    if(state[SDL_SCANCODE_RIGHT])
-        game_state->player.x += 10;
-    // Движение вверх
-    if(state[SDL_SCANCODE_UP])
-        game_state->player.y -= 10;
-    // Движение вниз
-    if(state[SDL_SCANCODE_DOWN])
-        game_state->player.y += 10;
+    else if(state[SDL_SCANCODE_RIGHT])
+        game->player.x += 10;
+    // // Движение вверх
+    // if(state[SDL_SCANCODE_UP])
+    //     game->player.y -= 10;
+    // // Движение вниз
+    // else if(state[SDL_SCANCODE_DOWN])
+    //     game->player.y += 10;
 
     return quit;
 }
 
+void game_process(GameState *game) {
+    Player *player = &game->player;
+    player->y += player->dy;
+
+    player->dy += GRAVITY;
+
+}
+
+// Обработка коллизий
+void game_collision(GameState *game) {
+    // Проверка коллизий на столкновение с уступами
+    for(int i = 0; i < 100; i++) {
+
+        // Получение данных
+        float mx = game->player.x, my = game->player.y;
+        float mw = game->player.w, mh = game->player.h;
+        float bx = game->ledges[i].x, by = game->ledges[i].y;
+        float bw = game->ledges[i].w, bh = game->ledges[i].h;
+
+        // И пошла математика
+        // Если задел
+        if (my + mh > by && my < by + bh) {
+            // Правая сторона
+            if (mx < bx + bw && mx + mw > bx + bw) {
+                game->player.x = bx + bw;
+                mx = bx + bw;
+            }
+            // Левая сторона
+            else if (mx + mw > bx && mx < bx) {
+                game->player.x = bx - mw;
+                mx = bx - mw;
+            }
+        }
+
+        // Если задел 2
+        if (mx + mw > bx && mx < bx + bw) {
+            // Верхняя сторона
+            if (my < by + bh && my > by) {
+                game->player.y = by + bh;
+                game->player.dy = 0;
+                game->player.on_ledge = 1;
+            }
+            // Нижняя сторона
+            else if (my + mh > by && my < by) {
+                game->player.y = by - mh;
+                game->player.dy = 0;
+                game->player.on_ledge = 1;
+            }
+        }
+    }
+}
+
+
 // Отрисовка
-int game_render(GameState *game_state) {
+int game_render(GameState *game) {
 
-    // Задать цвет отрисовки (синий)
-    SDL_SetRenderDrawColor(game_state->rndr,
-        150,  // red
-        150,  // green
-        150,  // blue
-        255  // alpha
-    );
+    // Задать цвет отрисовки (серый)
+    SDL_SetRenderDrawColor(game->rndr, 150, 150, 150, 255);
 
-    // Очистка экрана (из-за цвета отрисовки будет синим)
-    SDL_RenderClear(game_state->rndr);
+    // Очистка экрана (из-за цвета отрисовки будет серым)
+    SDL_RenderClear(game->rndr);
 
     // Задать цвет отрисовки (белый)
-    SDL_SetRenderDrawColor(game_state->rndr, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(game->rndr, 255, 255, 255, 255);
 
-    // Создание и отрисовка прямогульника
-    // SDL_Rect rect = {
-    //     game_state->player.x, // x
-    //     game_state->player.y, // y
-    //     game_state->player.width, // width
-    //     game_state->player.height // height
-    // };
-    // SDL_RenderFillRect(game_state->rndr, &rect);
+    // Создание и отрисовка уступов
+    for(int i = 0; i < 100; i++) {
+        SDL_Rect ledge_rect = {game->ledges[i].x, game->ledges[i].y, game->ledges[i].w, game->ledges[i].h};
+        SDL_RenderCopy(game->rndr, game->textures.ledge, NULL, &ledge_rect); // отрисовка текстуры
+    }
 
-    // Отрисовка текстуры игрока
-    SDL_Rect player_rect = {
-        game_state->player.x, // x
-        game_state->player.y, // y
-        game_state->player.width, // width
-        game_state->player.height // height
-    };
-    SDL_RenderCopy(
-        game_state->rndr,
-        game_state->player.texture,
-        NULL,
-        &player_rect
-    );
+    // Создание и отрисовка текстуры игрока
+    SDL_Rect player_rect = {game->player.x, game->player.y, game->player.w, game->player.h};
+    // SDL_RenderFillRect(game->rndr, &rect); // отрисовка прямоугольника
+    // SDL_RenderCopy(game->rndr, game->textures.player_frames[0], NULL, &player_rect); // отрисовка текстуры
+    SDL_RenderCopyEx(game->rndr, game->textures.player_frames[0], NULL, &player_rect, 0, NULL, 0); // отрисовка текстуры
 
+    // Создание и отрисовка звёзд
+    // for(int i = 0; i < 100; i++) {
+    //     SDL_Rect star_rect = {game->stars[i].x, game->stars[i].y, 32, 32};  // создание прямоугольника
+    //     SDL_RenderCopy(game->rndr, game->textures.star, NULL, &star_rect); // отрисовка текстуры
+    // }
 
     // Отрисовка объектов на окне
-    SDL_RenderPresent(game_state->rndr);
+    SDL_RenderPresent(game->rndr);
 
     return 0;
 }
@@ -109,56 +240,54 @@ int main(int argc, char *argv[]) {
     int quit = 0; // игровой цикл
 
     SDL_Window *window = NULL; // Объявление окна
-    SDL_Surface *surface = NULL; // Поверхность
+    GameState game; // Структура Состояние игры
 
-    GameState game_state; // Структура Состояние игры
-    game_state.player.x = 320-50;
-    game_state.player.y = 240-45;
-    game_state.player.width = 100;
-    game_state.player.height = 95;
-
-    // Инициализация SDL2
-    SDL_Init(SDL_INIT_VIDEO);
+    // Инициализация 
+    SDL_Init(SDL_INIT_VIDEO); // SDL2
+    srand((int)time(NULL)); // случайные числа
 
     // Создание окна
-    window = SDL_CreateWindow(
-        "SDL",                   // название окна
-        SDL_WINDOWPOS_UNDEFINED, // стартовая pos x
-        SDL_WINDOWPOS_UNDEFINED, // стартовая pos y
-        640,                     // ширина
-        480,                     // высота
-        0                        // флаги
-    );
+    window = SDL_CreateWindow(  "SDL",                   // название окна
+                                SDL_WINDOWPOS_UNDEFINED, // стартовая pos x
+                                SDL_WINDOWPOS_UNDEFINED, // стартовая pos y
+                                640,                     // ширина
+                                480,                     // высота
+                                0                        // флаги
+                                );
     // Создание визуализатора
-    game_state.rndr = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    game.rndr = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    // Загрузка изображения
-    surface = IMG_Load("assets/star.png");
-    // Создание текстуры
-    game_state.player.texture = SDL_CreateTextureFromSurface(game_state.rndr, surface);
-    // Очистка поверхности
-    SDL_FreeSurface(surface);
+    // Загрузка данных
+    load_game(&game);
 
     /* Игровой цикл */
     while(!quit) {
 
         /* Обработка событий */
-        quit = game_events(&game_state);
+        quit = game_events(&game);
+
+        game_process(&game);
+
+        /* Обработка коллизий */
+        game_collision(&game);
 
         /* Отрисовка */
-        game_render(&game_state);
+        game_render(&game);
 
         /* FPS */
         SDL_Delay(DELAY);
     }
 
     // Уничтожение текстуры
-    SDL_DestroyTexture(game_state.player.texture);
+    SDL_DestroyTexture(game.textures.player_frames[0]);
+    SDL_DestroyTexture(game.textures.player_frames[1]);
+    SDL_DestroyTexture(game.textures.ledge);
+    SDL_DestroyTexture(game.textures.star);
 
     // Закрытие и уничтожение окна
     SDL_DestroyWindow(window);
     // Закрытие и уничтожение визуализатора
-    SDL_DestroyRenderer(game_state.rndr);
+    SDL_DestroyRenderer(game.rndr);
 
     // Выход из SDL2
     SDL_Quit();
